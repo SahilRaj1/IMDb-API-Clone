5bf85485a7fe1049540deb98b03ea3bac61ad787
SahilRaj1 <raj24sahil@gmail.com>
2023-01-05 00:20:13 +0530

    video 31

--- watchlist/api/urls.py
+++ watchlist/api/urls.py
@@ -1,11 +1,18 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from watchlist.api import views
+
+router = DefaultRouter()
+router.register(r'platforms', views.StreamPlatformViewSet, basename='platform')
 
 urlpatterns = [
     path('watchlist/', views.WatchListAV.as_view(), name='movies'),
     path('watchlist/<int:pk>/', views.WatchListDetailAV.as_view(), name='movie'),
-    path('platforms/', views.StreamPlatformAV.as_view(), name='platforms'),
-    path('platforms/<int:pk>/', views.StreamPlatformDetailAV.as_view(), name='platform'),
+
+    path('', include(router.urls)),
+    # path('platforms/', views.StreamPlatformAV.as_view(), name='platforms'),
+    # path('platforms/<int:pk>/', views.StreamPlatformDetailAV.as_view(), name='platform'),
+
     path('watchlist/<int:pk>/reviews/', views.ReviewList.as_view(), name='reviews'),
     path('watchlist/<int:pk>/review-create/', views.ReviewCreate.as_view(), name='reviews'),
     path('watchlist/reviews/<int:pk>/', views.ReviewDetail.as_view(), name='review'),
--- watchlist/api/views.py
+++ watchlist/api/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework import mixins
 from rest_framework import generics
+from rest_framework import viewsets
 from watchlist.models import WatchList, StreamPlatform, Review
 from watchlist.api.serializers import WatchListSerializer, StreamPlatformSerializer, ReviewSerializer
 
@@ -125,61 +126,75 @@ class WatchListDetailAV(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-# Routes for all streaming platforms
-class StreamPlatformAV(APIView):
+# Routes for streaming platforms
+class StreamPlatformViewSet(viewsets.ViewSet):
 
-    # endpoint to fetch all platforms
-    def get(self, request):
-        platforms = StreamPlatform.objects.all()
-        serializer = StreamPlatformSerializer(platforms, many=True)
+    def list(self, request):
+        queryset = StreamPlatform.objects.all()
+        serializer = StreamPlatformSerializer(queryset, many=True)
         return Response(serializer.data)
 
-    # endpoint to add a new platform
-    def post(self, request):
-        serializer = StreamPlatformSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-# Routes for a specific platform
-class StreamPlatformDetailAV(APIView):
-
-    # endpoint to fetch a specific platform by id
-    def get(self, request, pk):
-        try:
-            platform = StreamPlatform.objects.get(pk=pk)
-        except StreamPlatform.DoesNotExist:
-            return Response({"error": "Platform not found"}, status=status.HTTP_404_NOT_FOUND)
-
+    def retrieve(self, request, pk=None):
+        queryset = StreamPlatform.objects.all()
+        platform = generics.get_object_or_404(queryset, pk=pk)
         serializer = StreamPlatformSerializer(platform)
         return Response(serializer.data)
 
-    # endpoint to update a specific platform by id
-    def put(self, request, pk):
-        try:
-            platform = StreamPlatform.objects.get(pk=pk)
-        except StreamPlatform.DoesNotExist:
-            return Response({"error": "Platform not found"}, status=status.HTTP_404_NOT_FOUND)
-
-        serializer = StreamPlatformSerializer(platform, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(status=status.HTTP_202_ACCEPTED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    # endpoint to delete a specific platform by id
-    def delete(self, request, pk):
-        try:
-            platform = StreamPlatform.objects.get(pk=pk)
-        except StreamPlatform.DoesNotExist:
-            return Response({"error": "Platform not found"}, status=status.HTTP_404_NOT_FOUND)
-
-        platform.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+# # Routes for all streaming platforms
+# class StreamPlatformAV(APIView):
+#
+#     # endpoint to fetch all platforms
+#     def get(self, request):
+#         platforms = StreamPlatform.objects.all()
+#         serializer = StreamPlatformSerializer(platforms, many=True)
+#         return Response(serializer.data)
+#
+#     # endpoint to add a new platform
+#     def post(self, request):
+#         serializer = StreamPlatformSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(status=status.HTTP_201_CREATED)
+#         else:
+#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#
+# # Routes for a specific platform
+# class StreamPlatformDetailAV(APIView):
+#
+#     # endpoint to fetch a specific platform by id
+#     def get(self, request, pk):
+#         try:
+#             platform = StreamPlatform.objects.get(pk=pk)
+#         except StreamPlatform.DoesNotExist:
+#             return Response({"error": "Platform not found"}, status=status.HTTP_404_NOT_FOUND)
+#
+#         serializer = StreamPlatformSerializer(platform)
+#         return Response(serializer.data)
+#
+#     # endpoint to update a specific platform by id
+#     def put(self, request, pk):
+#         try:
+#             platform = StreamPlatform.objects.get(pk=pk)
+#         except StreamPlatform.DoesNotExist:
+#             return Response({"error": "Platform not found"}, status=status.HTTP_404_NOT_FOUND)
+#
+#         serializer = StreamPlatformSerializer(platform, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(status=status.HTTP_202_ACCEPTED)
+#         else:
+#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     # endpoint to delete a specific platform by id
+#     def delete(self, request, pk):
+#         try:
+#             platform = StreamPlatform.objects.get(pk=pk)
+#         except StreamPlatform.DoesNotExist:
+#             return Response({"error": "Platform not found"}, status=status.HTTP_404_NOT_FOUND)
+#
+#         platform.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 # # Routes for all movies
