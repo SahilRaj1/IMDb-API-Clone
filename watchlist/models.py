5e960121244b92a329edfe8b868ef78258e5c3a8
SahilRaj1 <raj24sahil@gmail.com>
2023-01-04 11:15:44 +0530

    video 22

--- watchlist/models.py
+++ watchlist/models.py
@@ -2,16 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class WatchList(models.Model):
-    title = models.CharField(max_length=40)
-    description = models.CharField(max_length=255)
-    active = models.BooleanField(default=True)
-    genre = models.CharField(max_length=30)
-    created = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.title
-
+# Watch lists
 class StreamPlatform(models.Model):
     name = models.CharField(max_length=30)
     about = models.CharField(max_length=150)
@@ -19,3 +10,15 @@ class StreamPlatform(models.Model):
 
     def __str__(self):
         return self.name
+
+# Streaming Platforms
+class WatchList(models.Model):
+    title = models.CharField(max_length=40)
+    description = models.CharField(max_length=255)
+    active = models.BooleanField(default=True)
+    platform = models.ForeignKey(StreamPlatform, on_delete=models.CASCADE, related_name="watchlist")
+    genre = models.CharField(max_length=30)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
