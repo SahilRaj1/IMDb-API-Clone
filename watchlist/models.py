3b338de682a06d272b2f37efeb956cc35348ac3e
SahilRaj1 <raj24sahil@gmail.com>
2023-01-04 12:34:02 +0530

    video 26

--- watchlist/models.py
+++ watchlist/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
@@ -11,6 +12,7 @@ class StreamPlatform(models.Model):
     def __str__(self):
         return self.name
 
+
 # Streaming Platforms
 class WatchList(models.Model):
     title = models.CharField(max_length=40)
@@ -22,3 +24,16 @@ class WatchList(models.Model):
 
     def __str__(self):
         return self.title
+
+
+# Reviews
+class Review(models.Model):
+    watchlist = models.ForeignKey(WatchList, on_delete=models.CASCADE, related_name="reviews")
+    rating = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    description = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    valid = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.rating} -> {self.watchlist.title}"
