d69eb4785c047f010b6d18a6581d40b19b4e6ba5
SahilRaj1 <raj24sahil@gmail.com>
2023-01-07 12:37:49 +0530

    video 51 revert

--- user/api/views.py
+++ user/api/views.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework import status
-from rest_framework_simplejwt.tokens import RefreshToken
+# from rest_framework_simplejwt.tokens import RefreshToken
 from user.api.serializers import RegistrationSerializer
 from user import models
 
@@ -20,13 +20,13 @@ def registration_view(request):
             data['response'] = "Registration successful"
             data['username'] = account.username
             data['email'] = account.email
-            # token = Token.objects.get(user=account).key
-            # data['token'] = token
-            refresh = RefreshToken.for_user(user)
-            data['token'] = {
-                                'refresh': str(refresh),
-                                'access': str(refresh.access_token),
-                            }
+            token = Token.objects.get(user=account).key
+            data['token'] = token
+            # refresh = RefreshToken.for_user(user)
+            # data['token'] = {
+            #                     'refresh': str(refresh),
+            #                     'access': str(refresh.access_token),
+            #                 }
         else:
             data = serializer.errors
 
