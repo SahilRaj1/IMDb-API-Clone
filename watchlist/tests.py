68abcb54896d89081ee7b53c7066d9a2324c29aa
SahilRaj1 <raj24sahil@gmail.com>
2023-01-08 21:52:07 +0530

    video 64

--- watchlist/tests.py
+++ watchlist/tests.py
@@ -1,3 +1,35 @@
+from django.contrib.auth.models import User
 from django.urls import reverse
+
 from rest_framework import status
 from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
+
+from watchlist.api import serializers
+from watchlist import models
+
+class StreamPlatformTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username="admin", password="admin")
+        self.token = Token.objects.get(user__username=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        self.stream = models.StreamPlatform.objects.create(name="Netflix", about="#1 Platform", website="https://www.netflix.com")
+
+    def test_platform_create(self):
+        data = {
+            "name": "Netflix",
+            "about": "#1 Streaming Platform",
+            "website": "https://netflix.com"
+        }
+        response = self.client.post(reverse('platform-list'), data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_platform_list(self):
+        response = self.client.get(reverse('platform-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_platform_ind(self):
+        response = self.client.get(reverse('platform-detail', args=(self.stream.id,)))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
